0a0e2de00b0e3c1a86d9e1f46b178a775a5f84c7
Rahul Mukherjee <139303113+ALMOSTGOD1999@users.noreply.github.com>
2025-02-26 14:58:02 +0530

    done

---
+++ theme-switcher/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div>theme switcher</div>;
+}
+
+export default App;
